11167af18b2d404708927b35c5ad4c8ce4067055
JullioSanntos <JulioSantos@Hotmail.com>
2026-05-28 10:25:00 -0400

    feat: updated connection string to Azure SQL and successfully applied migrations

--- QuestPocBackend/QuestPocBackend/Program.cs
+++ QuestPocBackend/QuestPocBackend/Program.cs
@@ -19,7 +19,7 @@ public class Program {
                 });
         });
         builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         var app = builder.Build();
@@ -27,13 +27,11 @@ public class Program {
         // Comment this out for local dev to prevent 307 redirect CORS short-circuits
         // app.UseHttpsRedirection();
 
-        // 2. Apply your explicit named CORS policy BEFORE authorization or routing
+        // Apply your explicit named CORS policy BEFORE authorization or routing
         app.UseCors(questCorsPolicy);
 
-        // 3. Evaluate Authorization rules on the allowed origin request
         app.UseAuthorization();
 
-        // 4. Map the endpoints to the Controllers
         app.MapControllers();
 
         app.Run();
